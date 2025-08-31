a29c832078235da99db36d1710b07c202dd1f029
Vishnu Deep PandurangaRao <143198224+vdspw@users.noreply.github.com>
2025-08-30 20:44:13 -0700

    Create alwaysblock1.v
    inside the always block the left side before the = is the "reg" type.
    outside the always block the left side is a wire; 
    Reg -- does not need assign keyword but wire does need assign keyword.

---
+++ alwaysblock1.v
@@ -0,0 +1,16 @@
+/* Build an AND gate using both an assign statement and a combinational always block. */
+
+// synthesis verilog_input_version verilog_2001
+module top_module(
+    input a, 
+    input b,
+    output wire out_assign,
+    output reg out_alwaysblock
+);
+    always@(*)
+        begin
+           out_alwaysblock = a &&b;
+        end
+    assign out_assign = out_alwaysblock;
+endmodule
+
